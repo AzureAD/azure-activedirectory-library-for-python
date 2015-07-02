4a079dabed828b70bae0ad345b2491b268b968f0
Christopher Wilcox <git@crwilcox.com>
2015-07-02 10:22:51 -0700

    MIT License Headers

--- setup.py
+++ setup.py
@@ -1,4 +1,30 @@
 ﻿#!/usr/bin/env python
+#------------------------------------------------------------------------------
+#
+# Copyright (c) Microsoft Corporation. 
+# All rights reserved.
+# 
+# This code is licensed under the MIT License.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files(the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions :
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#------------------------------------------------------------------------------
 
 from setuptools import setup
 
@@ -20,9 +46,9 @@ setup(
     name='adal',
     version='0.1.0',
     description='The ADAL for Python library makes it easy for python application to authenticate to Azure Active Directory (AAD) in order to access AAD protected web resources.',
-    license='Apache 2',
-    author='Microsoft Open Technologies Inc',
-    author_email='msopentech@microsoft.com',
+    license='MIT',
+    author='Microsoft Corporation',
+    author_email='ptvshelp@microsoft.com',
     url='https://github.com/AzureAD/azure-activedirectory-library-for-python-priv',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -32,7 +58,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'License :: OSI Approved :: MIT License',  'License :: OSI Approved :: Apache Software License'
+        'License :: OSI Approved :: MIT License',
     ],
     packages=['adal'],
     install_requires=[

b1b5590ca179bb62423bee2f4663dc4dc37f6d8f
Rich Randall <rrandall@microsoft.com>
2015-10-03 17:34:25 -0700

    Last minute fix ups before publishing.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 ﻿#!/usr/bin/env python
 #------------------------------------------------------------------------------
 #
-# Copyright (c) Microsoft Corporation. 
+# Copyright (c) Microsoft Corporation.
 # All rights reserved.
-# 
+#
 # This code is licensed under the MIT License.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files(the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions :
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
@@ -48,8 +48,7 @@ setup(
     description='The ADAL for Python library makes it easy for python application to authenticate to Azure Active Directory (AAD) in order to access AAD protected web resources.',
     license='MIT',
     author='Microsoft Corporation',
-    author_email='ptvshelp@microsoft.com',
-    url='https://github.com/AzureAD/azure-activedirectory-library-for-python-priv',
+    url='https://github.com/AzureAD/azure-activedirectory-library-for-python',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python',

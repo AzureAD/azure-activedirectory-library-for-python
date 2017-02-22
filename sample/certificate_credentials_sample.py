83ce2adaf1a9b1ca7ed99f1dc7c62ea9eb81717f
Devin Villegas <dvillegas@luc.id>
2017-02-21 18:38:06 -0600

    Corrected usage in comment of sample code

--- sample/certificate_credentials_sample.py
+++ sample/certificate_credentials_sample.py
@@ -6,11 +6,11 @@ import adal
 
 def turn_on_logging():
     logging.basicConfig(level=logging.DEBUG)
-    #or, 
+    #or,
     #handler = logging.StreamHandler()
     #adal.set_logging_options({
     #    'level': 'DEBUG',
-    #    'handler': handler 
+    #    'handler': handler
     #})
     #handler.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
 
@@ -20,8 +20,8 @@ def get_private_key(filename):
     return private_pem
 
 #
-# You can provide account information by using a JSON file. Either 
-# through a command line argument, 'python sample.js parameters.json', or
+# You can provide account information by using a JSON file. Either
+# through a command line argument, 'python sample.py parameters.json', or
 # specifying in an environment variable of ADAL_SAMPLE_PARAMETERS_FILE.
 # privateKeyFile must contain a PEM encoded cert with private key.
 # thumbprint must be the thumbprint of the privateKeyFile.
@@ -32,7 +32,7 @@ def get_private_key(filename):
 #   "thumbprint" : 'C15DEA8656ADDF67BE8031D85EBDDC5AD6C436E1',
 #   "privateKeyFile" : 'ncwebCTKey.pem'
 # }
-parameters_file = (sys.argv[1] if len(sys.argv) == 2 else 
+parameters_file = (sys.argv[1] if len(sys.argv) == 2 else
                    os.environ.get('ADAL_SAMPLE_PARAMETERS_FILE'))
 sample_parameters = {}
 if parameters_file:
@@ -43,7 +43,7 @@ else:
     raise ValueError('Please provide parameter file with account information.')
 
 
-authority_url = (sample_parameters['authorityHostUrl'] + '/' + 
+authority_url = (sample_parameters['authorityHostUrl'] + '/' +
                  sample_parameters['tenant'])
 RESOURCE = '00000002-0000-0000-c000-000000000000'
 
@@ -54,9 +54,9 @@ context = adal.AuthenticationContext(authority_url)
 key = get_private_key(sample_parameters['privateKeyFile'])
 
 token = context.acquire_token_with_client_certificate(
-    RESOURCE, 
-    sample_parameters['clientId'], 
-    key, 
+    RESOURCE,
+    sample_parameters['clientId'],
+    key,
     sample_parameters['thumbprint'])
 
 print('Here is the token:')
